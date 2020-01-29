0fc063a1d2e90befc0ecdf4e7756e3f094fb8b0e
yuan_xy <yuan_xin_yu@hotmail.com>
2020-01-29 22:13:30 +0800

    change HashValue instance to bytes

--- libra/hasher.py
+++ libra/hasher.py
@@ -34,7 +34,7 @@ class HashValue(DelegateT):
     @classmethod
     def random_hash(cls):
         import random
-        return [random.randint(0, Uint8.max_value) for x in range(HashValue.LENGTH)]
+        return bytes([random.randint(0, Uint8.max_value) for x in range(HashValue.LENGTH)])
 
 
 def uint8_to_bits(uint8):
@@ -86,16 +86,16 @@ def create_literal_hash(word):
     assert len(arr) <= HashValue.LENGTH
     for _i in range(len(arr), HashValue.LENGTH):
         arr.append(0)
-    return arr
+    return bytes(arr)
 
 ACCUMULATOR_PLACEHOLDER_HASH = create_literal_hash("ACCUMULATOR_PLACEHOLDER_HASH")
 SPARSE_MERKLE_PLACEHOLDER_HASH = create_literal_hash("SPARSE_MERKLE_PLACEHOLDER_HASH")
 PRE_GENESIS_BLOCK_ID = create_literal_hash("PRE_GENESIS_BLOCK_ID")
-GENESIS_BLOCK_ID = [
+GENESIS_BLOCK_ID = bytes([
         0x5e, 0x10, 0xba, 0xd4, 0x5b, 0x35, 0xed, 0x92, 0x9c, 0xd6, 0xd2, 0xc7, 0x09, 0x8b, 0x13,
         0x5d, 0x02, 0xdd, 0x25, 0x9a, 0xe8, 0x8a, 0x8d, 0x09, 0xf4, 0xeb, 0x5f, 0xba, 0xe9, 0xa6,
         0xf6, 0xe4,
-    ]
+    ])
 
 
 def tst_only_hash(obj, clazz=None) -> HashValue:
--- test/test_hasher.py
+++ test/test_hasher.py
@@ -27,9 +27,9 @@ def test_default_hasher():
 
 
 def test_placeholder_hash():
-    assert ACCUMULATOR_PLACEHOLDER_HASH == [65, 67, 67, 85, 77, 85, 76, 65, 84, 79, 82, 95, 80, 76, 65, 67, 69, 72, 79, 76, 68, 69, 82, 95, 72, 65, 83, 72, 0, 0, 0, 0]
-    assert SPARSE_MERKLE_PLACEHOLDER_HASH == [83, 80, 65, 82, 83, 69, 95, 77, 69, 82, 75, 76, 69, 95, 80, 76, 65, 67, 69, 72, 79, 76, 68, 69, 82, 95, 72, 65, 83, 72, 0, 0]
-    assert PRE_GENESIS_BLOCK_ID == [80, 82, 69, 95, 71, 69, 78, 69, 83, 73, 83, 95, 66, 76, 79, 67, 75, 95, 73, 68, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert ACCUMULATOR_PLACEHOLDER_HASH == bytes([65, 67, 67, 85, 77, 85, 76, 65, 84, 79, 82, 95, 80, 76, 65, 67, 69, 72, 79, 76, 68, 69, 82, 95, 72, 65, 83, 72, 0, 0, 0, 0])
+    assert SPARSE_MERKLE_PLACEHOLDER_HASH == bytes([83, 80, 65, 82, 83, 69, 95, 77, 69, 82, 75, 76, 69, 95, 80, 76, 65, 67, 69, 72, 79, 76, 68, 69, 82, 95, 72, 65, 83, 72, 0, 0])
+    assert PRE_GENESIS_BLOCK_ID == bytes([80, 82, 69, 95, 71, 69, 78, 69, 83, 73, 83, 95, 66, 76, 79, 67, 75, 95, 73, 68, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 
 def test_common_prefix_nibbles_len():
@@ -41,3 +41,6 @@ def test_common_prefix_nibbles_len():
     assert common_prefix_bits_len(b"hello", b"HELLO") == 2
     assert common_prefix_bits_len(b"h2", b"\xff2") == 0
     assert common_prefix_bits_len(b"hello", b"hello") == 40
+
+def test_random_hash():
+    HashValue.check_value(HashValue.random_hash())
