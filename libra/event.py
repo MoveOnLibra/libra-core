eaba670ac1ada8029a8adcfe6d846ac0cd538474
yuan_xy <yuan_xin_yu@hotmail.com>
2020-03-18 20:42:47 +0800

    gen random EventKey

--- libra/event.py
+++ libra/event.py
@@ -2,6 +2,10 @@ from canoser import DelegateT, Struct, Uint64, Uint8, BytesT
 from libra.account_address import Address
 
 
+def random_eventkey():
+    return bytes([Uint8.random() for _x in range(EVENT_KEY_LENGTH)])
+
+
 EVENT_KEY_LENGTH = 40
 
 class EventKey(DelegateT):
@@ -14,6 +18,11 @@ class EventKey(DelegateT):
         output_bytes = lhs + rhs
         return output_bytes
 
+    @classmethod
+    def random(cls):
+        # return cls.new_from_address(Address.random(), Uint64.random())
+        return bytes([Uint8.random() for _x in range(EVENT_KEY_LENGTH)])
+
 
 
 class EventHandle(Struct):
@@ -21,4 +30,8 @@ class EventHandle(Struct):
         ('count', Uint64),#Number of events in the event stream.
         ('key', EventKey) #The associated globally unique key that is used as the key to the EventStore.
     ]
+
+    @classmethod
+    def random_handle(cls, count):
+        return cls(count, EventKey.random())
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("libra/version.py", "r") as fp:
 
 
 install_requires=[
-        'canoser>=0.7.7',
+        'canoser>=0.7.11',
         'protobuf',
         'grpcio',
         'more-itertools',
