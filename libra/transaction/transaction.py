196d774a42a02d9b4e9afdd7c5fa5fcc6112a2fc
yuan_xy <yuan_xin_yu@hotmail.com>
2020-04-10 10:07:19 +0800

    change genesis tx to WaypointWriteSet

--- libra/transaction/transaction.py
+++ libra/transaction/transaction.py
@@ -1,6 +1,6 @@
 from canoser import RustEnum, Uint64
 from libra.transaction.signed_transaction import SignedTransaction
-from libra.transaction.write_set import WriteSet
+from libra.transaction.change_set import ChangeSet
 from libra.block_metadata import BlockMetadata
 from libra.hasher import gen_hasher
 from libra.proto_helper import ProtoHelper
@@ -10,7 +10,7 @@ Version = Uint64
 class Transaction(RustEnum):
     _enums = [
         ('UserTransaction', SignedTransaction),
-        ('WriteSet', WriteSet),
+        ('WaypointWriteSet', ChangeSet),
         ('BlockMetadata', BlockMetadata)
     ]
 
