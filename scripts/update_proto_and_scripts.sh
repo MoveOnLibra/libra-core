a67df5a7dc2faffa62b9ebe62968b39cace5f420
yuan_xy <yuan_xin_yu@hotmail.com>
2020-04-10 08:50:51 +0800

    update proto files

--- scripts/update_proto_and_scripts.sh
+++ scripts/update_proto_and_scripts.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-cd ..
-mkdir testnet
-cd testnet
-git clone https://github.com/libra/libra.git
-cd libra
-git checkout origin/testnet
+# cd ..
+# mkdir testnet
+# cd testnet
+# git clone https://github.com/libra/libra.git
+# cd libra
+# git checkout origin/testnet
 
 
 #scp xn@an1:~/libra/
 
-rm ../../libra-core/proto/*.proto
-find . -name *.proto | xargs cp -t ../../libra-core/proto/
+cd ../testnet_libra
+rm ../libra-core/proto/*.proto
+find . -name *.proto | xargs cp -t ../libra-core/proto/
 #rpl "shared/mempool_status" "mempool_status" proto/mempool.proto
 
 
-cd ../../libra-core/
+cd ../libra-core/
 ./scripts/generate_protobuf.sh
